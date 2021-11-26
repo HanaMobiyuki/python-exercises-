4e50a18b0a600cc15a86d6fb319bc7f6c061576e
Laura-Razzy <lauramacuer@gmail.com>
2021-11-26 13:45:02 -0300

    oops lol 2

--- 10.py
+++ 10.py
@@ -1,6 +1,13 @@
 # Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
-n = int(input("Input and int : "))
+n = int(input("Input an int : "))
 n1 = int("%s" % n)
 n2 = int("%s%s" % (n,n))
 n3 = int("%s%s%s" % (n,n,n))
-print(n1+n2+n3)
+sum = n1+n2+n3
+
+print("n = ", n1)
+print("nn = ", n2)
+print("nnn = ", n3)
+print("n+nn+nnn = ", sum)
+
+#Homework from Laura: make this same program, make it so it so it only accepts numerical values between 0 and 9. If the input is something that is not in that range, make the program ask for a proper input instead of crashing.
