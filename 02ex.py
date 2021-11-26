c8a5d47e468cf8dbf59179bd59e67f2a3c09c4a1
Laura-Razzy <lauramacuer@gmail.com>
2021-11-26 13:43:17 -0300

    oops lol

---
+++ 02ex.py
@@ -0,0 +1,7 @@
+# Write a Python program to get the Python version you are using.
+import sys
+version = sys.version
+version_info = sys.version_info
+
+print("Python version:", version)
+print("Version info:", version_info)
